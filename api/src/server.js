a07649317c7f4a492f13b833a5cbb3c7e7cdffdd
shunjid <shunjid.rahman@gmail.com>
2020-11-16 17:50:24 +0600

    demonstrated route params in express

--- api/src/server.js
+++ api/src/server.js
@@ -8,6 +8,10 @@ app.get("/hello", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}!`);
+});
+
 app.post("/hello", (req, res) => {
   res.send(`Hello ${req.body.name}`);
 });
